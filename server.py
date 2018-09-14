0e82a0b8a16e10e4e7bdd27974c35f730f9015b4
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-14 04:29:29 +0100

    Redo the same thing for Maldies #17

--- server.py
+++ server.py
@@ -26,7 +26,14 @@ def addSaleBlock():
         json.dump(blockchain,data,indent=4)
     return jsonify(blockchain),200
 
-
+@app.route('/addMaladyBlock', methods = ['GET'])
+def addMaladyBlock():
+    file = open('maladies.json')
+    blockchain = json.load(file)
+    blockchain.append(json.loads(request.args.get('block')))
+    with open('maladies.json','w') as data:    
+        json.dump(blockchain,data,indent=4)
+    return jsonify(blockchain),200
     
 
 app.run(host = '0.0.0.0', port = 5000)
